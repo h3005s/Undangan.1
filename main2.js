141f69aa15d3f3455f106c0f20c8721b8c406a1e
h3005s <haidil.s.3005@gmail.com>
2024-10-07 12:44:12 +0700

    Update main2.js

--- main2.js
+++ main2.js
@@ -4,24 +4,24 @@ document.addEventListener("DOMContentLoaded", function() {
     const p3 = document.querySelector(".p3");
     const gambar = document.querySelectorAll(".gambar");
 
-    // Fungsi untuk mengatur animasi gambar
+    /
     p3.addEventListener("animationend", function() {
-        gambar[0].style.display = "block"; // Tampilkan gambar Puti
-        gambar[0].style.animation = "munculDariKanan 0.5s forwards"; // Mulai animasi gambar Puti
+        gambar[0].style.display = "block"; 
+        gambar[0].style.animation = "munculDariKanan 0.5s forwards"; 
 
         gambar[0].addEventListener("animationend", function() {
-            gambar[1].style.display = "block"; // Tampilkan gambar Cincin
-            gambar[1].style.animation = "membesar 0.5s forwards"; // Mulai animasi gambar Cincin
+            gambar[1].style.display = "block"; 
+            gambar[1].style.animation = "membesar 0.5s forwards"; 
             
             gambar[1].addEventListener("animationend", function() {
-                gambar[2].style.display = "block"; // Tampilkan gambar Fauzan
-                gambar[2].style.animation = "munculDariKiri 0.5s forwards"; // Mulai animasi gambar Fauzan
+                gambar[2].style.display = "block"; 
+                gambar[2].style.animation = "munculDariKiri 0.5s forwards"; 
 
-                // Setelah animasi Fauzan selesai, tunggu 1 detik, lalu pindah halaman
+                
                 gambar[2].addEventListener("animationend", function() {
                     setTimeout(function() {
-                        window.location.href = "p3.html"; // Pindah ke halaman p3.html
-                    }, 1000); // Tunggu 1 detik
+                        window.location.href = "p3.html"; 
+                    }, 1000); 
                 });
             });
         });
